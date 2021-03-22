34c9c8da584204161f02c860166c6f9bd8b347cb
Dana <35933787+danawoodruff@users.noreply.github.com>
2021-03-22 09:51:10 -0500

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,8 @@
-// * Your data markers should reflect the magnitude of the earthquake by their size and and depth of the earth quake by color. Earthquakes with higher magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
-//    * **HINT** the depth of the earth can be found as the third coordinate for each earthquake.
+
 //    * Create a legend that will provide context for your map data.
 
 // Store our API endpoint inside queryUrl
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// Perform a GET request to the query URL
-d3.json(queryUrl).then(data => {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
 
 function createFeatures(earthquakeData) {
 
@@ -71,37 +64,16 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
-
-  // function to assign colors for legend and markers
-  function getColor(d) {
-    return d > 4 ? '#D61A46' :
-        d > 3 ? '#FB9902' :
-        d > 2.5 ? '#FDED2A' :
-        d > 2 ? '#CBE432' :
-        d > 1.5 ? '#559E54' :
-        d > 1 ? '#1258DC' :
-                '#7114B8';
 }
 
-let legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function(myMap) {
-    const div = L.DomUtil.create('div', 'info legend')
-    const depth = [0, .5, 1, 5, 10, 30]
-    const labels = []
-
-    for (let i = 0; i < depth.length; i++) {
-        div.innerHTML +=
-        '<i style="background:' + getColor(depth[i] + 1) + '"></i>' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+ ');
-    }
-    return div
-};
-legend.addTo(myMap);
-
-}
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(data => {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+// });
 
 // Create a GeoJSON layer containing the features array on the earthquakeData object
-// Run the onEachFeature function once for each piece of data in the array
+// Run the onEachFeature function for each piece of data in the array
 // Assign marker size based on magnitude of quake
 function createFeatures(eqdata) {
   function onEachFeature(feature, layer) {
@@ -113,7 +85,7 @@ function createFeatures(eqdata) {
       pointToLayer: function(feature, latlng) {
           // Marker radius dependent on magnitude of quake
           let radius = feature.properties.mag * 5;
-          console.log(feature.geometry.coordinates[2]);
+          
           // Colors mimic USGS color scale
           // if (feature.properties.mag > 4) {
           if (feature.geometry.coordinates[2] > 30) {
@@ -142,9 +114,53 @@ function createFeatures(eqdata) {
               fillColor: fillcolor,
               fillOpacity: .5,
               weight: 1
-          });
-      }
-  });
+          });  
+      }  
+  });   //end of "layertoMap" code block"
 
 createMap(layerToMap);
-}
+
+// Set up the legend
+let legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  let depth = [0, .5, 1, 5, 10, 30];
+  let colors = ['#D61A46','#FB9902','#FDED2A','#CBE432','#559E54','#1258DC','#7114B8']
+  
+  // Add min & max
+  let legendInfo = "<h1>Earthquake Depth</h1>" +
+    "<div class=\"labels\">" +
+      "<div class=\"min\">" + depth[0] + "</div>" +
+      "<div class=\"max\">" + depth[depth.length - 1] + "</div>" +
+    "</div>";
+
+  div.innerHTML = legendInfo;
+
+  depth.forEach(function(depth, index) {
+    labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  });
+
+  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  return div;
+};
+
+// Adding legend to the map
+legend.addTo(myMap);
+
+}   //end of "createFeatures" code block"
+});
+
+
+
+// legend.onAdd = function(myMap) {
+//     const div = L.DomUtil.create('div', 'info legend')
+//     const depth = [0, .5, 1, 5, 10, 30]
+//     const labels = []
+
+//     for (let i = 0; i < depth.length; i++) {
+//         div.innerHTML +=
+//         '<i style="background:' + getColor(depth[i] + 1) + '"></i>' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+ ');
+//     }
+//     return div
+// };
